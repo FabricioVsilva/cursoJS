1bfa7256ba6c4d5d2e37c1efcf86da14b8bf7f1a
bowie <marketing@letscode.com.br>
2025-06-22 11:32:49 -0300

    Trabalhando com Strings e Numbers

---
+++ aula14/index.js
@@ -0,0 +1,13 @@
+let num1 = 10; // Number
+let num2 = 5; // Number
+
+// console.log(num1.toString() + num2);
+// num1 = num1.toString();
+// console.log(num1.toString(2));
+// console.log(num1.toFixed(2));
+// console.log(Number.isInteger(num1));
+// console.log(Number.IsNaN(temp));
+// let temp = num1 + '5';
+// console.log(temp);
+
+
