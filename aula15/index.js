8eb8e1d4117a0a9d25c13a0befe291a9f59a39bc
bowie <marketing@letscode.com.br>
2025-06-22 20:25:53 -0300

    Trabalhando com módulo Math

---
+++ aula15/index.js
@@ -0,0 +1,10 @@
+// Math.floor: Arredonda para baixo;
+// Math.ceil: Arredonda para cima;
+// Math.round: Arredonda para o mais próximo;
+// Math.max: Pega o maior número;
+// Math.min: Pega o menor número;
+// Math.random: Gera números aleatórios entre 0 e 1;
+// Math.pow: Eleva o número;
+// Raiz quadrada: ** 0.5
+
+console.log(Math.round(Math.random() * 10));
